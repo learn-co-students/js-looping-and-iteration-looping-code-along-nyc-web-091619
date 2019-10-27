7727599d07993008d54a37fa71d62bf0ceec8e45
Michelle Lee <mlee092@Michelles-MacBook.local>
2019-10-27 17:59:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+
+// writeCards()
+// returns an array of thank you messages for each name provided to the function:
+
+function writeCards(namesArray, event) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+
+
+// countDown()
+// invokes console.log once for each number, counting down from the number provided to zero:
+// logs each number when counting down, starting from the number provided:
+
+  function countDown(startingNumber) {
+    while (startingNumber > 0) {
+      console.log(startingNumber);
+      startingNumber -= 1;
+    }
+    console.log(startingNumber);
+  }
