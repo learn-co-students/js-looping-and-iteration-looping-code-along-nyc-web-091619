705146ba218abe4cdb1acef24eeb8046d4313300
maryfarner18 <maryfarner@gmail.com>
2019-10-27 22:00:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+
+function writeCards(names){
+    let messages = [];
+    for (let i=0; i < names.length; i++){
+       messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+       
+    }
+
+    return messages;
+}
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num);
+        num--;
+    }
+
+}
+
+countDown(4);
+
