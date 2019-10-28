a9308aa4bad68883c56ea40465bddbda86a6c3b5
Jonathan Tan <you@example.com>
2019-10-27 21:28:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+   const newArray= [] 
+
+    for (let i = 0; i < names.length; i++) {
+       newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
