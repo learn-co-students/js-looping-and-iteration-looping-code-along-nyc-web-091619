647cdb4ebab3b40ee5fee61ce7a50ba4b2144a2b
Liam <liam.healy1@marist.edu>
2019-10-29 08:41:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, type) {
+    const responses = [];
+    for (let i = 0; i < names.length; i++) {
+        responses.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`);
+    }
+    return responses;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        --num;
+    }
+}
