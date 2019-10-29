aa0f8055a203cd9877d5e7c86c91f6f27186a019
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-10-29 10:34:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(nameArr, event) {
+    let thankYouMessages = []
+    for(let i = 0; i < nameArr.length; i++) {
+        thankYouMessages.push(`Thank you, ${nameArr[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+}
+
+function countDown(num) {
+    for(let i = num; i >= 0; i--){
+        console.log(i);
+    }
+}
